576bcd064ab0cf89692e997ba47a90f501e514d2
ahmedkhalf <ahmedkhalf567@gmail.com>
2024-08-03 23:24:31 -0400

    Changes
    a very good commit name

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,17 @@
 import { defineConfig } from 'astro/config';
+import rehypeSlug from 'rehype-slug'
+import rehypeAutolinkHeadings from 'rehype-autolink-headings'
+
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [
+    mdx({
+      rehypePlugins: [
+        rehypeSlug,
+        rehypeAutolinkHeadings
+      ],
+    }),
+  ],
+});
