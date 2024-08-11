a0695d606c6668c5fa8b1ebe5532d2a233782179
Ahmed Khalf <ahmedkhalf567@gmail.com>
2024-08-11 20:51:36 +0000

    Add rss feed

---
+++ src/pages/rss.xml.ts
@@ -0,0 +1,20 @@
+import type { APIContext } from 'astro';
+import rss from '@astrojs/rss';
+import getBlogEntries from '../lib/getBlogEntries';
+import createSlug from '../lib/createSlug';
+
+export async function GET(context: APIContext) {
+  const blog = await getBlogEntries();
+
+  return rss({
+    title: "Ahmed's Blog",
+    description: "Ahmed's blog",
+    site: context.site ?? "",
+    items: blog.map((post) => ({
+      title: post.data.title,
+      pubDate: post.data.date_published,
+      link: `/posts/${createSlug(post.data.title)}/`
+    })),
+    customData: `<language>en-ca</language>`,
+  });
+}
