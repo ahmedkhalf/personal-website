5d86fc861a3bc3465d816ed788bac9edecde5c7b
Ahmed Khalf <ahmedkhalf567@gmail.com>
2024-08-17 23:37:28 +0000

    change title of blog post

--- src/content/config.ts
+++ src/content/config.ts
@@ -4,6 +4,7 @@ const blogCollection = defineCollection({
     type: 'content',
     schema: z.object({
         title: z.string(),
+        permatitle: z.string().optional(),
         draft: z.boolean().optional(),
         date_published: z.date().optional(),
         date_edited: z.date().optional(),
